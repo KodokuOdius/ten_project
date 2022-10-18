0b1d0c45cc434b02f5d2f124c4ef52424e59d6b7
KodokuOdius <alanfoxincoolctranica@gmail.com>
2022-10-18 22:31:02 +0300

    create counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './index.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [counter, setCounter] = useState<number>(0);
+
+    const onPlus = () => {
+        setCounter(counter + 1);
+    };
+    const onMinus = () => {
+        setCounter(counter - 1);
+    };
+
+    return (
+        <div className="App">
+            <div>
+                <h2>Счетчик:</h2>
+                <h1>{counter}</h1>
+                <button className="minus" onClick={onMinus}>- Минус</button>
+                <button className="plus" onClick={onPlus}>Плюс +</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
